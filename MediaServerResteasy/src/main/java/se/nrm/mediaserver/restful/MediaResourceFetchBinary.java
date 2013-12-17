7a17dc9d6db2025332a52b63f39942e024c71eac
Ingimar Erlingsson <ingimar.erlingsson@nrm.se>
2013-12-17 20:08:27 +0100

    Resize on the fly using a convenient library from -> http://projects.coobird.net/thumbnailator/

--- MediaServerResteasy/src/main/java/se/nrm/mediaserver/restful/MediaResourceFetchBinary.java
+++ MediaServerResteasy/src/main/java/se/nrm/mediaserver/restful/MediaResourceFetchBinary.java
@@ -16,11 +16,13 @@ import javax.ws.rs.core.Response;
 //
 import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.imageio.ImageIO;
+import net.coobird.thumbnailator.Thumbnails;
 import org.apache.tika.Tika;
 import se.nrm.mediaserver.util.PathHelper;
 
@@ -43,24 +45,27 @@ public class MediaResourceFetchBinary {
         return returnFile(repositoryFile);
     }
 
-    /**
-     * testar en teknik, ska inte sparas ....
-     *
-     * @param fileName
-     * @return
-     */
     @GET
-    @Path("/stream/small/{uuid}")
-    public Response getMediaSmall(@PathParam("uuid") String fileName) {
-        String dynPath = getDynamicPath(fileName);
-        String file = dynPath.concat(fileName);
-        System.out.println("filename " + fileName);
-        // test to save, you could return the thumb without saving
-        if (!file.endsWith(".thumb")) {
-            savesThumbfil(file);
+    @Path("/streaming/{uuid}")
+    @Produces("image/jpeg")
+    public byte[] getT(@PathParam("uuid") String uuid) {
+        System.out.println("uuid " + uuid);
+        String dynPath = getDynamicPath(uuid);
+        String filename = dynPath.concat(uuid);
+
+        File fileHandle = new File(filename);
+        ByteArrayOutputStream bo = new ByteArrayOutputStream(2048);
+
+        try {
+            BufferedImage originalImage = ImageIO.read(fileHandle);
+            int type = originalImage.getType() == 0 ? BufferedImage.TYPE_INT_ARGB : originalImage.getType();
+            BufferedImage transformedImage = Thumbnails.of(originalImage).size(150, 150).asBufferedImage();
+            ImageIO.write(transformedImage, "jpeg", bo);
+        } catch (IOException ex) {
+            Logger.getLogger(MediaResourceFetchBinary.class.getName()).log(Level.SEVERE, null, ex);
         }
-        File repositoryFile = new File(file);
-        return returnFile(repositoryFile);
+
+        return bo.toByteArray();
     }
 
     private static Response returnFile(File file) {
@@ -75,42 +80,6 @@ public class MediaResourceFetchBinary {
         } catch (IOException ioEx) {
             return Response.status(Response.Status.NOT_FOUND).build();
         }
-    }
-
-    private void savesThumbfil(String file) {
-
-        // returnera on-the-fly , kan inte köra in BufferedImage hit ->  return Response.ok(new FileInputStream(file)).build();
-        try {
-
-            BufferedImage originalImage = ImageIO.read(new File(file));
-            int type = originalImage.getType() == 0 ? BufferedImage.TYPE_INT_ARGB : originalImage.getType();
-
-            BufferedImage resizeImageJpg = resizeImage150x150(originalImage, type);
-            ImageIO.write(resizeImageJpg, "jpg", new File(file + ".thumb"));
-            File cacheDirectory = ImageIO.getCacheDirectory();
-
-        } catch (IOException e) {
-            System.out.println(e.getMessage());
-            e.getCause();
-        }
-    }
-
-    /**
-     * Todo kunna välja från ett urval av bilder,
-     *
-     * @param originalImage
-     * @param type
-     * @return
-     */
-    private static BufferedImage resizeImage150x150(BufferedImage originalImage, int type) {
-        int IMG_WIDTH = 150;
-        int IMG_HEIGHT = 150;
-        BufferedImage resizedImage = new BufferedImage(IMG_WIDTH, IMG_HEIGHT, type);
-        Graphics2D g = resizedImage.createGraphics();
-        g.drawImage(originalImage, 0, 0, IMG_WIDTH, IMG_HEIGHT, null);
-        g.dispose();
-
-        return resizedImage;
     }
 
     private String getDynamicPath(String uuid) {
